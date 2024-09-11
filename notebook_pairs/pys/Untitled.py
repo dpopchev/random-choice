85ec188ea0f1178606c46dd9ca199356e0134ec7
Dimitar Popchev <dpopchev@gmail.com>
2024-09-11 06:58:25 +0300

    feat: next data method generation

--- notebook_pairs/pys/Untitled.py
+++ notebook_pairs/pys/Untitled.py
@@ -13,9 +13,32 @@
 # ---
 
 # %%
-from random_choice.records import WeightedRecord
+from random_choice.records import ProbabilityRecord
+from collections import Counter
 
 # %%
-WeightedRecord(10, 0.2)
+sample = [ProbabilityRecord(-1, 0.33), ProbabilityRecord(0, 0.33), ProbabilityRecord(1, 0.33)]
+
+# %%
+c = Counter((s.data for s in sample))
+
+# %%
+c.update((s.data for s in sample))
+
+# %%
+c
+
+# %%
+3*0.33 - 1 
+
+# %%
+from itertools import cycle
+from random import choices
+
+# %%
+c = cycle(choices([r.data for r in sample], weights=[r.probability for r in sample], k=10))
+
+# %%
+next(c)
 
 # %%
